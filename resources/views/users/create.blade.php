d8b6e52b21764a56e879a74b91dc7ae59aae9f96
yanbrpj <inteelbr@hotmail.com>
2023-03-02 11:58:45 -0300

    Aula 15 - Validar Dados do Form

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -4,11 +4,19 @@
 @section('content')
     <h1>Criar Usuário</h1>
     
+    @if ($errors->any())
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li class="error" style="color:#FF0000">{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+
     <form action="{{ route('users.store') }}" method="post">
         @csrf
 
-        <input type="text" name="name" placeholder="Nome" id="name">
-        <input type="email" name="email" placeholder="Email" id="email">
+        <input type="text" name="name" placeholder="Nome" id="name" value="{{ old('name') }}">
+        <input type="email" name="email" placeholder="Email" id="email" value="{{ old('email') }}">
         <input type="password" name="password" placeholder="Senha" id="password">
 
         <button type="submit">Cadastrar</button>
