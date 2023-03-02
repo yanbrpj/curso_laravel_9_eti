61dd50a7cb20ba533e7f01c341cf77e901a57346
yanbrpj <inteelbr@hotmail.com>
2023-03-02 11:04:48 -0300

    Aula 10 - Listar Registros

--- app/Http/Controllers/Usercontroller.php
+++ app/Http/Controllers/Usercontroller.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class Usercontroller extends Controller
 {
     public function index() {
-        return view('users.index');
+        $users = User::get();
+
+        return view('users.index', [
+            'users' => $users,
+        ]);
     }
 
     public function show($id) {
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,1 +1,9 @@
-Listagem de usuários
+<h1>Listagem de usuários</h1>
+
+<ul>
+    @foreach ($users as $user)
+        <li>
+            {{ $user->name }} - {{ $user->email }}
+        </li>
+    @endforeach
+</ul>
